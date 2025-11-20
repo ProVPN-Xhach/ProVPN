93dc104a1949957069183d99934347e3219c4a54
ProVPN-Xhach <provpntunnel293@gmail.com>
2025-11-20 10:40:42 +0800

    Update pro25.sh

--- pro25.sh
+++ pro25.sh
@@ -331,7 +331,7 @@ DOMAIN="rhyuunet.biz.id"
 IPVPS=$(curl -s ipv4.icanhazip.com)
 
 # === Generate Subdomain Random ===
-SUBDOMAIN=$(cat /dev/urandom | tr -dc a-z0-9 | head -c 5)
+SUBDOMAIN="do11"
 RECORD="$SUBDOMAIN.$DOMAIN"
 
 # === Get Zone ID dari Cloudflare ===
